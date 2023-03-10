0fb799a9792afd469e7e59e2b886021780baba17
AmirrOmidvar <127245392+AmirrOmidvar@users.noreply.github.com>
2023-03-10 23:57:01 +0330

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AmirOmidvarTamrin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string answer = "";
+            Console.WriteLine("Are you married?");
+            answer = Console.ReadLine();
+            switch (answer)
+            {
+                case ("yes"):
+                    Console.WriteLine("Do you love Your Wife?");
+                    answer = Console.ReadLine();
+                    switch (answer)
+                    {
+                        case("yes"):
+                            Console.WriteLine("Do you have any child?");
+                            answer = Console.ReadLine();
+                            switch (answer)
+                            {
+                                case ("yes"):
+                                    Console.WriteLine("do you love them?");
+                                    answer = Console.ReadLine();
+                                    switch (answer)
+                                    {
+                                        case ("yes"):
+                                            Console.WriteLine("do they love you?");
+                                            answer = Console.ReadLine();
+                                            switch (answer)
+                                            {
+                                                case ("yes"):
+                                                    Console.WriteLine("Great!");
+                                                    break;
+                                                case ("no"):
+                                                    Console.WriteLine("You SHould be a better father");
+                                                    break;
+                                                default:
+                                                    Console.WriteLine("Invalid");
+                                                    break;
+
+
+                                            }
+                                            break;
+                                        case ("no"):
+                                            Console.WriteLine("You have to love them so that they love you");
+                                            break;
+                                        default:
+                                            Console.WriteLine("Invalid");
+                                            break;
+
+                                    }
+                                    break;
+                                case ("no"):
+                                    Console.WriteLine("Please have a baby");
+                                    break;
+                                default:
+                                    Console.WriteLine("Invalid");
+                                    break;
+                            }
+                            break;
+                        case ("no"):
+                            Console.WriteLine("You should love her to have a better life.");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid");
+                            break;
+
+                    }
+                    break;
+                case ("no"):
+                    Console.WriteLine("Lets get married");
+                    break;
+                default: Console.WriteLine("Invalid");
+                    break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
